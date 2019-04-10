e100eff79e3a4d6dc168d6e056b7372f7a090656
Felipe Cano <felipecanol@gmail.com>
2019-04-10 17:57:38 -0500

    Se creo el servicio de consulta básica desde la base de datos

--- src/main/java/info/proadmintierra/rdm/controllers/ParcelQueryRestController.java
+++ src/main/java/info/proadmintierra/rdm/controllers/ParcelQueryRestController.java
@@ -1,53 +1,72 @@
 package info.proadmintierra.rdm.controllers;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Map;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import org.springframework.util.ResourceUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import info.proadmintierra.rdm.queries.BasicQuery;
+import info.proadmintierra.rdm.drivers.Postgres;
+
 /**
  * ParcelQueryRestController
  */
 @RestController
 @RequestMapping("/query")
-@CrossOrigin(origins = {"http://localhost:4200","*"})
+@CrossOrigin(origins = { "http://localhost:4200", "*" })
 public class ParcelQueryRestController {
 
+    @Value("${rdm.schema}")
+    private String rdmSchema;
+    @Value("${spring.datasource.url}")
+    private String connectionString;
+    @Value("${spring.datasource.username}")
+    private String connectionUser;
+    @Value("${spring.datasource.password}")
+    private String connectionPassword;
+    @Value("${spring.datasource.driver-class-name}")
+    private String classForName;
+
     @GetMapping(value = "/parcel", produces = { "application/json" })
-    public Map<String, Object> getParcelBasicInfo(@RequestParam(required = false) String nupre,
+    public String getParcelBasicInfo(@RequestParam(required = false) String nupre,
             @RequestParam(required = false) String cadastralCode, @RequestParam(required = false) String fmi) {
-
+        String sql = "";
         try {
-            File file = ResourceUtils.getFile("classpath:static/info_basica.json");
-
-            InputStream inputStream = new FileInputStream(file);
-            ObjectMapper mapper = new ObjectMapper();
-            try {
-                Map<String, Object> jsonMap = mapper.readValue(inputStream, Map.class);
-
-                return jsonMap;
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } catch (FileNotFoundException e) {
+            Postgres conn = new Postgres();
+            conn.connect(this.connectionString, this.connectionUser, this.connectionPassword, this.classForName);
+            sql = BasicQuery.getQuery(this.rdmSchema, null, fmi, cadastralCode, null, false, false);
+            String response = conn.query(sql);
+            conn.disconnect();
+            if (response != null)
+                return response;
+            else
+                return "{\"error\":\"No se encontraron registros.\"}";
+        } catch (Exception e) {
             e.printStackTrace();
+            return "{\"error\":\"" + e.getMessage() + "\"}";
         }
-
-        return null;
-
     }
 
-   
+    @GetMapping(value = "/parcel/geometry", produces = { "application/json" })
+    public String getParcelBasicInfo(@RequestParam(required = false) Integer id) {
+        String sql = "";
+        try {
+            Postgres conn = new Postgres();
+            conn.connect(this.connectionString, this.connectionUser, this.connectionPassword, this.classForName);
+            sql = "select st_asgeojson(terreno.poligono_creado) from " + rdmSchema + ".terreno where terreno.t_id = " + id;
+            System.out.println("SQL: "+sql);
+            String response = conn.query(sql);
+            conn.disconnect();
+            if (response != null)
+                return response;
+            else
+                return "{\"error\":\"No se encontraron registros.\"}";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "{\"error\":\"" + e.getMessage() + "\"}";
+        }
+    }
+
 }
---
+++ src/main/java/info/proadmintierra/rdm/drivers/Postgres.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.proadmintierra.rdm.drivers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Postgres {
+
+    private Connection c = null;
+    private Statement stmt = null;
+
+    public String query(String sql) {
+        String response = "";
+        try {
+            this.stmt = this.c.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            rs.next();
+            response = rs.getString(1);
+            stmt.close();
+            c.commit();
+            return response;
+        } catch (SQLException ex) {
+            Logger.getLogger(Postgres.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return response;
+    }
+
+    public boolean connect(String connectionString, String connectionUser, String connectionPassword,
+            String classForName) {
+        this.disconnect();
+        try {
+            Class.forName(classForName);
+            c = DriverManager.getConnection(connectionString, connectionUser, connectionPassword);
+            c.setAutoCommit(false);
+            return true;
+        } catch (Exception ex) {
+            Logger.getLogger(Postgres.class.getName()).log(Level.SEVERE, null, ex);
+            this.disconnect();
+        }
+        return false;
+    }
+
+    public void disconnect() {
+        if (this.c != null) {
+            try {
+                this.c.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(Postgres.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        this.c = null;
+    }
+
+}
