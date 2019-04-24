f3d7b915fd647cd557d05547332db40c4beeb82e
Felipe Cano <felipecanol@gmail.com>
2019-04-24 13:44:09 -0500

    Se implementaron las consultas de información económica, legal, física y de ficha

--- src/main/java/info/proadmintierra/rdm/controllers/ParcelQueryRestController.java
+++ src/main/java/info/proadmintierra/rdm/controllers/ParcelQueryRestController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import info.proadmintierra.rdm.queries.BasicQuery;
+import info.proadmintierra.rdm.queries.EconomicQuery;
+import info.proadmintierra.rdm.queries.IGACPropertyRecordCardQuery;
+import info.proadmintierra.rdm.queries.LegalQuery;
+import info.proadmintierra.rdm.queries.PhysicalQuery;
 import info.proadmintierra.rdm.drivers.Postgres;
 
 /**
@@ -55,8 +59,11 @@ public class ParcelQueryRestController {
         try {
             Postgres conn = new Postgres();
             conn.connect(this.connectionString, this.connectionUser, this.connectionPassword, this.classForName);
-            sql = "select st_asgeojson(terreno.poligono_creado) from " + rdmSchema + ".terreno where terreno.t_id = " + id;
-            System.out.println("SQL: "+sql);
+            sql = "select st_asgeojson(ST_Transform(terreno.poligono_creado, 3857)) from " + rdmSchema
+                    + ".terreno where terreno.t_id = " + id;
+            // sql = "select st_asgeojson(ST_Transform(terreno.poligono_creado, 4326)) from
+            // " + rdmSchema + ".terreno where terreno.t_id = " + id;
+            System.out.println("SQL: " + sql);
             String response = conn.query(sql);
             conn.disconnect();
             if (response != null)
@@ -69,4 +76,83 @@ public class ParcelQueryRestController {
         }
     }
 
+    @GetMapping(value = "/parcel/economic", produces = { "application/json" })
+    public String getParcelEconomicInfo(@RequestParam(required = false) String nupre,
+            @RequestParam(required = false) String cadastralCode, @RequestParam(required = false) String fmi) {
+        String sql = "";
+        try {
+            Postgres conn = new Postgres();
+            conn.connect(this.connectionString, this.connectionUser, this.connectionPassword, this.classForName);
+            sql = EconomicQuery.getQuery(this.rdmSchema, null, fmi, cadastralCode, null, true, true);
+            String response = conn.query(sql);
+            conn.disconnect();
+            if (response != null)
+                return response;
+            else
+                return "{\"error\":\"No se encontraron registros.\"}";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "{\"error\":\"" + e.getMessage() + "\"}";
+        }
+    }
+
+    @GetMapping(value = "/parcel/legal", produces = { "application/json" })
+    public String getParcelLegalInfo(@RequestParam(required = false) String nupre,
+            @RequestParam(required = false) String cadastralCode, @RequestParam(required = false) String fmi) {
+        String sql = "";
+        try {
+            Postgres conn = new Postgres();
+            conn.connect(this.connectionString, this.connectionUser, this.connectionPassword, this.classForName);
+            sql = LegalQuery.getQuery(this.rdmSchema, null, fmi, cadastralCode, null);
+            String response = conn.query(sql);
+            conn.disconnect();
+            if (response != null)
+                return response;
+            else
+                return "{\"error\":\"No se encontraron registros.\"}";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "{\"error\":\"" + e.getMessage() + "\"}";
+        }
+    }
+
+    @GetMapping(value = "/parcel/physical", produces = { "application/json" })
+    public String getParcelPhysicalInfo(@RequestParam(required = false) String nupre,
+            @RequestParam(required = false) String cadastralCode, @RequestParam(required = false) String fmi) {
+        String sql = "";
+        try {
+            Postgres conn = new Postgres();
+            conn.connect(this.connectionString, this.connectionUser, this.connectionPassword, this.classForName);
+            sql = PhysicalQuery.getQuery(this.rdmSchema, null, fmi, cadastralCode, null, true);
+            String response = conn.query(sql);
+            conn.disconnect();
+            if (response != null)
+                return response;
+            else
+                return "{\"error\":\"No se encontraron registros.\"}";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "{\"error\":\"" + e.getMessage() + "\"}";
+        }
+    }
+
+    @GetMapping(value = "/parcel/igac_property_record_card", produces = { "application/json" })
+    public String getParcelIGACPropertyRecordCardInfo(@RequestParam(required = false) String nupre,
+            @RequestParam(required = false) String cadastralCode, @RequestParam(required = false) String fmi) {
+        String sql = "";
+        try {
+            Postgres conn = new Postgres();
+            conn.connect(this.connectionString, this.connectionUser, this.connectionPassword, this.classForName);
+            sql = IGACPropertyRecordCardQuery.getQuery(this.rdmSchema, null, fmi, cadastralCode, null, true);
+            String response = conn.query(sql);
+            conn.disconnect();
+            if (response != null)
+                return response;
+            else
+                return "{\"error\":\"No se encontraron registros.\"}";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "{\"error\":\"" + e.getMessage() + "\"}";
+        }
+    }
 }
